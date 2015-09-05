57365c259971b8ebe2d4fe8366ed1524847c59ec
frederickly_ah <fredericklyah@gmail.com>
2015-09-06 00:43:42 +1000

    seperate util isArray method to utils.js

--- routes/process.js
+++ routes/process.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var utils=require('./../js/common/utils');
+var fieldNames=require('./../js/common/constants');
 var router = express.Router();
 
 /* GET users listing. */
@@ -12,45 +13,46 @@ router.post('/', function(request, response, next) {
     }
 
     var result={'response':[]};
-    for (i = 0; i < input['payload'].length; i++) {
-        var item=input['payload'][i];
+    for (i = 0; i < input[fieldNames.PAYLOAD].length; i++) {
+        var item=input[fieldNames.PAYLOAD][i];
 
-        if(!item.hasOwnProperty('drm')){
+        if(!item.hasOwnProperty(fieldNames.DRM)){
             continue;
         }
-        if(!item.hasOwnProperty('episodeCount')){
+        if(!item.hasOwnProperty(fieldNames.EPISODECOUNT)){
             continue;
         }
 
-        if(item['drm']===true && item['episodeCount']>0){
+        if(item[fieldNames.DRM]===true && item[fieldNames.EPISODECOUNT]>0){
 
             var targetItem={};
 
-            if(!item.hasOwnProperty('image')){
+            if(!item.hasOwnProperty(fieldNames.IMAGE)){
                 console.warn('payload element does hot have property image');
                 continue;
             }
 
-            if(!item.hasOwnProperty('slug')){
+            if(!item.hasOwnProperty(fieldNames.SLUG)){
                 console.warn('payload element does hot have property slug');
                 continue;
             }
 
-            if(!item.hasOwnProperty('title')){
+            if(!item.hasOwnProperty(fieldNames.TITLE)){
                 console.warn('payload element does hot have property title');
                 continue;
             }
 
-            var image=item['image'];
 
-            if(!image.hasOwnProperty('showImage')){
+            var image=item[fieldNames.IMAGE];
+            if(!image.hasOwnProperty(fieldNames.SHOWIMAGE)){
                 continue;
             }
 
-            targetItem['image']=item['image']['showImage'];
-            targetItem['slug']=item['slug'];
-            targetItem['title']=item['title'];
-            result['response'].push(targetItem);
+
+            targetItem[fieldNames.IMAGE]=item[fieldNames.IMAGE][fieldNames.SHOWIMAGE];
+            targetItem[fieldNames.SLUG]=item[fieldNames.SLUG];
+            targetItem[fieldNames.TITLE]=item[fieldNames.TITLE];
+            result[fieldNames.RESPONSE].push(targetItem);
         }
 
 
@@ -62,24 +64,24 @@ router.post('/', function(request, response, next) {
 
     function isRequestValidated(req){
         var input = request.body;
-        if(!input.hasOwnProperty('payload')) {
-            console.log('Can not find payload!');
+        if(!input.hasOwnProperty(fieldNames.PAYLOAD)) {
+            console.warn('Can not find payload!');
             return false;
         }
 
-        if(!utils.isArray(input['payload'])){
-            console.log('Payload is not an array!');
+        if(!utils.isArray(input[fieldNames.PAYLOAD])){
+            console.warn('Payload is not an array!');
             return false;
         }
 
-        for (i = 0; i < input['payload'].length; i++) {
-            var item=input['payload'][i];
+        for (i = 0; i < input[fieldNames.PAYLOAD].length; i++) {
+            var item=input[fieldNames.PAYLOAD][i];
 
-            if (!item.hasOwnProperty('drm')) {
+            if (!item.hasOwnProperty(fieldNames.DRM)) {
                 console.warn('payload element does hot have property drm');
                 //continue;
             }
-            if (!item.hasOwnProperty('episodeCount')) {
+            if (!item.hasOwnProperty(fieldNames.EPISODECOUNT)) {
                 console.warn('payload element does hot have property episodeCount');
                 //continue;
             }
