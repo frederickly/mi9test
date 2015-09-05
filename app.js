2348bd1ad008f5a81d7d19359cb14fdb704a9d96
frederickly_ah <fredericklyah@gmail.com>
2015-09-06 01:14:45 +1000

    seperate util isArray method to utils.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 
 var routes = require('./routes/index');
-var process = require('./routes/process');
+var processor = require('./routes/process');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/process', process);
+app.use('/process', processor);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -61,6 +61,6 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Listening on port 3000...");
 });
